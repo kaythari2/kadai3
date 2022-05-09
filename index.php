03bdec6f5ddf0f0d534b7871760289a9ab458932
K <kaythari@genio.co.jp>
2022-05-09 14:02:26 +0900

    登録中

--- index.php
+++ index.php
@@ -4,12 +4,18 @@ require_once('header.php');
 
 <center id="container">
     <h1>画像アップロード</h1>
+    <span style="color: green;"><?php if (isset($_GET["status"]) && $_GET["status"] == "registered") {
+                                    echo "登録に成功しました。";
+                                } ?></span>
+    <span style="color: red;"><?php if (isset($_GET['e']) && !empty($_GET['e'])) {
+                                    echo $_GET['e'];
+                                } ?></span>
     <div class="wrapper">
         <div id="img-preview-1" class="image-preview">
         </div>
         <div class="form">
             <form action="upload_file.php" class="form_upload" method="POST" enctype="multipart/form-data">
-                <input type="file" id="file1" accept=".jpg" name="file1">
+                <input type="file" id="file1" name="file1">
                 <button type="submit" id="upload_img_1" name="upload_img_1">アプロード</button>
             </form>
         </div>
@@ -21,9 +27,9 @@ require_once('header.php');
         <div id="img-preview-2" class="image-preview">
         </div>
         <div class="form">
-            <form action="#" class="form_upload" method="POST" enctype="multipart/form-data">
-                <input type="file" name="file2" id="file2" accept=".jpg">
-                <button type="submit" id="upload_img_2">アプロード</button>
+            <form action="upload_file.php" class="form_upload" method="POST" enctype="multipart/form-data">
+                <input type="file" name="file2" id="file2">
+                <button type="submit" id="upload_img_2" name='upload_img_2'>アプロード</button>
             </form>
         </div>
     </div>
@@ -34,9 +40,9 @@ require_once('header.php');
         <div id="img-preview-3" class="image-preview">
         </div>
         <div class="form">
-            <form action="#" class="form_upload" method="POST"  enctype="multipart/form-data">
-                <input type="file" name="file3" id="file3" accept=".jpg">
-                <button type="submit" id="upload_img_3">アプロード</button>
+            <form action="upload_file.php" class="form_upload" method="POST" enctype="multipart/form-data">
+                <input type="file" name="file3" id="file3">
+                <button type="submit" id="upload_img_3" name='upload_img_3'>アプロード</button>
             </form>
         </div>
     </div>
--- upload_file.php
+++ upload_file.php
@@ -1,34 +1,68 @@
 <?php
-include_once('header.php');
+$tmp_directory = "tmp/";
+$imageName = (isset($_POST['upload_img_1'])) ? '001.jpg' : ((isset($_POST['upload_img_2'])) ? '002.jpg' : ((isset($_POST['upload_img_3'])) ? '003.jpg' : null));
+
+if (isset($_POST['register'])) {
+    $reged_directory = "registered/";
+    if(!file_exists($reged_directory)) {
+        mkdir($reged_directory);
+    }
+    $moved = rename($tmp_directory.$imageName, $reged_directory.$imageName); //should move the file
+    if($moved)  {
+        header("Location: index.php?status=registered");
+    } else {
+        header("Location: index.php?e=登録エラー");
+    }
+    return;
+}
+$fileName = (isset($_POST['upload_img_1'])) ? 'file1' : ((isset($_POST['upload_img_2'])) ? 'file2' : ((isset($_POST['upload_img_3'])) ? 'file3' : null));
+
+if (!file_exists($tmp_directory)) {
+    mkdir($tmp_directory);
+}
+$filepath = $tmp_directory . $imageName;
+$uploadOk = false;
+
+$file = $_FILES[$fileName];
+if ($file["size"] > 2000000) {
+    header("Location: index.php?e=画像は2MB以内で入力して下さい。");
+    return;
+}
+
+$target_file=basename($file["name"]);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+if ($imageFileType != "jpg") {
+    header("Location: index.php?e=拡張子はjpgで入力して下さい。");
+    return;
+}
+
+if (move_uploaded_file($_FILES[$fileName]["tmp_name"], $filepath)) {
+    // echo "<img src=" . $filepath . " height=auto width=300 />";
+    $uploadOk = true;
+} else {
+    header("Location: index.php?e=error");
+}
 ?>
+
 <center>
     <div id="content">
         <h1>確認画面</h1>
         <br>
-        <span>以下の画像を登録します。</span><br>
+        <span>以下の画像を登録します。</span>
+        <br>
+        <br>
         <form action="" method="post">
             <?php
-            if (isset($_POST['upload_img_1'])) {
-                $tmp_directory = "tmp/";
-                if(!file_exists($tmp_directory)){
-                    mkdir($tmp_directory);
-                }
-                $filepath = $tmp_directory . '001.jpg';
-
-                if (move_uploaded_file($_FILES["file1"]["tmp_name"], $filepath)) {
-                    echo "<img src=" . $filepath . " height=auto width=300 />";
-                } else {
-                    echo "Error !!";
-                }
+            if (isset($filepath) && $filepath != '') {
+                echo "<img src=" . $filepath . " height=auto width=300 />";
             }
             ?>
             <br>
+            <br>
             <a href="javascript:history.go(-1);"><input type="button" value="戻る"></a>
+            <br>
             <br>
             <input type="submit" name="register" value="登録">
         </form>
     </div>
 </center>
-<?php
-include_once('footer.php');
-?>
